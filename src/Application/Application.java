2099b3afd6d2afd8bb72ccdb4a215d465353a82e
Jack <scruffydevilliers@gmail.com>
2018-09-24 13:54:19 +0200

    Login Controller

---
+++ src/Application/Application.java
@@ -0,0 +1,44 @@
+package Application;
+
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Application extends javafx.application.Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public Scene createMainScene() {
+        VBox root = new VBox();
+        root.setFillWidth(true);
+        return new Scene(root);
+    }
+
+    public Stage createEditingStage(Stage owner) {
+        Stage stage = new Stage();
+
+        return stage;
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("deVill Chain");
+        Scene scene = createMainScene();
+        Stage editingStage = createEditingStage(primaryStage);
+        Controller controller = new loginController();
+
+
+        primaryStage.setTitle("Example 4");
+        primaryStage.setScene(controller.linkToScene(scene, editingStage));
+        primaryStage.show();
+    }
+}
---
+++ src/Application/Controller.java
@@ -0,0 +1,9 @@
+package Application;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public interface Controller {
+    public Scene linkToScene(Scene scene, Stage editing);
+
+}
